37dfa6c801bf428be64f5f5bd93015b42ce468c5
elitecarp12000 <112532225+elitecarp12000@users.noreply.github.com>
2022-09-30 12:04:36 +0900

    Add files via upload

---
+++ 0929/script/dom.js
@@ -0,0 +1,19 @@
+let title01 = document.getElementsByTagName('h3');
+console.log(title01);
+// for(let i=0;i<=title01.length;i++){
+//     title01.item(i).style.color="#00f";
+// }
+
+//2.id
+let p01 = document.getElementById('p01');
+
+
+//     p01.style.backgroundColor="#ff0";
+
+
+//3.class
+let title02 = document.getElementsByClassName('title02');
+for(let i=0;i<title02.length;i++){
+   title02.item(i).style.color ="f00"; 
+}
+
